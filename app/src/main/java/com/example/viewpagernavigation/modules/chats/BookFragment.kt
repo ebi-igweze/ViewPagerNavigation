0a8bee9c7b27818b6f3f352bdf2c6a9c2710ba41
ebi igweze <ebiigweze@ebis-MacBook-Pro.local>
2019-05-06 04:34:30 +0100

    updates all the views for each destination

--- app/src/main/java/com/example/viewpagernavigation/modules/chats/BookFragment.kt
+++ app/src/main/java/com/example/viewpagernavigation/modules/chats/BookFragment.kt
@@ -14,7 +14,7 @@ class BookFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false)
+        return inflater.inflate(R.layout.fragment_book, container, false)
     }
 
 
